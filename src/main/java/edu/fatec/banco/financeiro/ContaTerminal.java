8a58b724f15d0167349f8d4c2822c910faea630d
marikm <matsumotomarina3@gmail.com>
2024-07-08 14:54:28 -0300

    variaveis da classe contaTerminal criadas

--- src/main/java/edu/fatec/banco/financeiro/ContaTerminal.java
+++ src/main/java/edu/fatec/banco/financeiro/ContaTerminal.java
@@ -2,5 +2,41 @@ package edu.fatec.banco.financeiro;
 
 public class ContaTerminal {
 
+    private final Integer numero;
+    private final String agencia;
+    private final String nomeCliente;
+    private final Float saldo;
 
+    public ContaTerminal(Integer numero, String agencia, String nomeCliente, Float saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public Float getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "ContaTerminal{" +
+                "numero=" + numero +
+                ", agencia='" + agencia + '\'' +
+                ", nomeCliente='" + nomeCliente + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
 }
