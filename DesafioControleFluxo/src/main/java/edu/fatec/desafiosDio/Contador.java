36a7fadc04ca0a1d3fa5019cba9c550f25201efc
marikm <matsumotomarina3@gmail.com>
2024-07-11 11:54:15 -0300

    adicionando projeto de controle de fluxo

---
+++ DesafioControleFluxo/src/main/java/edu/fatec/desafiosDio/Contador.java
@@ -0,0 +1,27 @@
+package edu.fatec.desafiosDio;
+
+public class Contador {
+
+    public static void checarParametros(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if(parametroDois < parametroUm){
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    public static void contar(int parametroUm, int parametroDois) {
+        try{
+            checarParametros(parametroUm, parametroDois);
+            for(int i = 1; i <= parametroDois - parametroUm; i++){
+                System.out.println("Imprimindo o numero " + i);
+            }
+        } catch (ParametrosInvalidosException e){
+            System.out.println("O segundo parametro deve ser maior que o primeiro");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        contar(40, 30);
+    }
+
+}
---
+++ DesafioControleFluxo/src/main/java/edu/fatec/desafiosDio/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package edu.fatec.desafiosDio;
+
+public class ParametrosInvalidosException extends Exception{
+
+    public ParametrosInvalidosException(String mensagem){
+        super(mensagem);
+    }
+}
