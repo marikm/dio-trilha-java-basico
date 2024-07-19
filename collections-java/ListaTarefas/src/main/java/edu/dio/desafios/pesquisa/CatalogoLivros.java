b2956e5608ed2cdf7eb171c22b18cf6fbf544f0f
marikm <matsumotomarina3@gmail.com>
2024-07-19 09:11:46 -0300

    classe livro e construtor da lista de livros criados

---
+++ collections-java/ListaTarefas/src/main/java/edu/dio/desafios/pesquisa/CatalogoLivros.java
@@ -0,0 +1,15 @@
+package edu.dio.desafios.pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+
+    List<Livro> listaLivros;
+
+    public CatalogoLivros() {
+        this.listaLivros = new ArrayList<>();
+    }
+
+
+}
---
+++ collections-java/ListaTarefas/src/main/java/edu/dio/desafios/pesquisa/Livro.java
@@ -0,0 +1,25 @@
+package edu.dio.desafios.pesquisa;
+
+public class Livro {
+    private final String titulo;
+    private final String autor;
+    private final Integer anoPublicacao;
+
+    public Livro(String titulo, String autor, Integer anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+}
